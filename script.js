35392cb7e33a7214bb01889b8396049e4cb9ba5b
DemianBretinAlvarez <demianbretinalvarez@orange.fr>
2024-04-05 14:26:07 +0200

    récupération code postal

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ input_cp.addEventListener("input", function(){recup_cp(input_cp.value)});
 function recup_cp(cp) {
     if (cp.length == 5) {
         console.log(cp);
-        let url = 'https://geo.api.gouv.fr/communes?codePostal=${cp}';
+        let url = 'https://geo.api.gouv.fr/communes?codePostal=' + cp;
+        let liste_commune = []
+        console.log(url);
         try {
             fetch(url)
-            
+            .then((resp) => resp.json())
+            .then(function(communes) {
+                communes.forEach((element) => liste_commune.push(element));
+            })
         } catch (error) {
             console.log("ça ne marche pas");
         }
