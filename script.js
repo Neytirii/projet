567875dd5ea44665149699ec4aee1ac8a1edff37
DemianBretinAlvarez <demianbretinalvarez@orange.fr>
2024-05-07 15:49:09 +0200

    modif mineure

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const input_cp = document.getElementById("code-postal");
 const menu_commune = document.getElementById("menu-deroulant")
+const bouton = document.getElementById("envoyer")
 menu_commune.style.display = "none"
+bouton.style.display = "none"
 
 input_cp.addEventListener("input", async () => {
     let code_postal = input_cp.value;
@@ -8,6 +10,7 @@ input_cp.addEventListener("input", async () => {
         try {
             let donnee_commune = await recup_communes(code_postal);
             affiche_commune(donnee_commune)
+            let liste_commune = menu_commune.querySelectorAll("option")
         } catch (error) {
             console.log("Erreur lors de la récupération des données de communes", error);
             throw error;
@@ -34,5 +37,18 @@ function affiche_commune(donnee) {
     donnee.forEach(commune => {
         menu_commune.innerHTML += `<option>${Object.values(commune)[0]}</option>`
     });
-    menu_commune.style.display = "initial"
+    menu_commune.style.display = "initial";
+    bouton.style.display = "initial";
+}
+
+async function affiche_meteo(cp) {
+    try {
+        let reponse = await fetch(`https://api.meteo-concept.com/api/forecast/daily?token=4cfe199cf47ef5bbf0473b2e30e9d48fd90561cf03ef5a2532c69799f32410e8&insee=${cp}`)
+        let donne_meteo = await reponse.json()
+        console.table(donne_meteo) 
+    } catch (error) {
+        console.log("Une erreur est survenu", error)
+        throw error
+    }
+    
 }
